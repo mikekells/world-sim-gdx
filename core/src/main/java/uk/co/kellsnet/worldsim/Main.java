c98ea0e0b4925c08708349bc6b6d8abef7875844
Mike Kells <mikekells@proton.me>
2026-03-12 11:09:39 +0000

    Added camera

--- core/src/main/java/uk/co/kellsnet/worldsim/Main.java
+++ core/src/main/java/uk/co/kellsnet/worldsim/Main.java
@@ -1,6 +1,7 @@
 package uk.co.kellsnet.worldsim;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -13,11 +14,23 @@ public class Main extends ApplicationAdapter {
     private Texture pillarTexture;
     private TileRenderer tileRenderer;
     private TileMap tileMap;
+    private OrthographicCamera camera;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         tileMap = new TileMap();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 600);
+
+        int worldWidth = tileMap.getWidth() * 32;
+        int worldHeight = tileMap.getHeight() * 32;
+
+        camera.position.set(worldWidth / 2f, worldHeight / 2f, 0);
+        camera.update();
+
+        batch.setProjectionMatrix(camera.combined);
 
         Pixmap wallPixmap = new Pixmap(32, 32, Pixmap.Format.RGBA8888);
         wallPixmap.setColor(1, 1, 1, 1);
@@ -37,6 +50,9 @@ public class Main extends ApplicationAdapter {
     @Override
     public void render() {
         ScreenUtils.clear(0f, 0f, 0f, 1f);
+
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
         tileRenderer.render(batch, tileMap);
